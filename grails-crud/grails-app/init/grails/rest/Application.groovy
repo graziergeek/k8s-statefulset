48c9b7a695211ae6cbfdb7752612dae80a37ddd5
jeffcunningham <cunninghamj@objectcomputing.com>
2018-10-15 17:46:29 -0500

    Some alteration on connecting to mongodb replica set.

--- grails-crud/grails-app/init/grails/rest/Application.groovy
+++ grails-crud/grails-app/init/grails/rest/Application.groovy
@@ -2,9 +2,33 @@ package grails.rest
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import grails.util.*
 
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
+//        new StartupGrailsApp(Application).run(Application, args)
     }
 }
+
+//@InheritConstructors
+//class StartupGrailsApp extends GrailsApp {
+//    @Override
+
+//    protected void logStartupInfo(boolean isRoot) {
+//        // Show default info.
+//        super.logStartupInfo(isRoot)
+//
+//        // And add some extra logging information.
+//        // We use the same logger if we get the
+//        // applicationLog property.
+//        if (applicationLog.debugEnabled) {
+//            final metaInfo = Metadata.getCurrent()
+//            final String grailsVersion = GrailsUtil.grailsVersion
+//            applicationLog.debug "Running with Grails v${grailsVersion}"
+//
+//            final sysprops = System.properties
+//            applicationLog.debug "Running on ${sysprops.'os.name'} v${sysprops.'os.version'}"
+//        }
+//    }
+//}
